f770ed03c22ce5af25fbd5e9147d0f999d61352e
Muhammad Hazim <hazimutalib96@gmail.com>
2020-11-23 19:53:15 +0800

    new updates

--- app.py
+++ app.py
@@ -55,4 +55,58 @@ def line_graph():
 st.subheader("Time-Series Graph of Malaysia's Trade Perfomance from 2013 to 2019")
 line_graph()
 
+def single_graph_bar(column,sort):
+    fig, ax = plt.subplots(1, 1)
+    if (column == 'COUNTRY') or (column == 'SITC 2 DIGIT'):
+        k = st.slider('Top:', 1, df[column].nunique(), 10, key='22')
+        df.groupby(column)[show].sum().sort_values(by=sort, ascending=False).iloc[:k].plot(kind='bar', ax=ax, figsize=(12, 8))
+        st.pyplot(fig)
+        st.dataframe(df.groupby(column)[show].sum().sort_values(by=sort, ascending=False).iloc[:k])
+    else:
+        df.groupby(column)[show].sum().sort_values(by=sort, ascending=False).plot(kind='bar', ax=ax, figsize=(12, 8))
+        st.pyplot(fig)
+        st.dataframe(df.groupby(column)[show].sum().sort_values(by=sort, ascending=False))
+
+def bi_graph_bar(column_1, column_2, i, sort):
+    fig, ax = plt.subplots(1, 1)
+    if (column_1 == 'COUNTRY') or (column_1 == 'SITC 2 DIGIT'):
+        k = st.slider('Top:', 1, df[column_1].nunique(), 10, key='23')
+        df[df[column_2] == i].groupby(column_1)[show].sum().sort_values(by=sort, ascending=False).iloc[:k].plot(kind='bar', ax=ax, figsize=(12, 8), title=column_2 + ': ' + str(i));
+    else:
+        df[df[column_2] == i].groupby(column_1)[show].sum().sort_values(by=sort, ascending=False).plot(kind='bar', ax=ax, figsize=(12, 8), title=column_2 + ': ' + str(i));
+    st.pyplot(fig)
+
+def multiple_graph_bar(column_1, column_2, i, column_3, j, sort):
+    fig, ax = plt.subplots(1, 1)
+    if (column_1 == 'COUNTRY') or (column_1 == 'SITC 2 DIGIT'):
+        k = st.slider('Top:', 1, df[column_1].nunique(), 10, key='24')
+        df[(df[column_2] == i) & (df[column_3] == j)].groupby(column_1)[show].sum().sort_values(by=sort, ascending=False).iloc[:k].plot(kind='bar', ax=ax, figsize=(12, 8), title=column_2 + ': ' + str(i) + ' and ' + column_3 + ': ' + str(j));
+    else:
+        df[(df[column_2] == i) & (df[column_3] == j)].groupby(column_1)[show].sum().sort_values(by=sort, ascending=False).plot(kind='bar', ax=ax, figsize=(12, 8), title=column_2 + ': ' + str(i) + ' and ' + column_3 + ': ' + str(j));
+    st.pyplot(fig)
+
+def bar_graph():
+    feature = st.selectbox('Feature:', ['YEAR', 'COUNTRY', 'SITC 1 DIGIT', 'SITC 2 DIGIT'])
+    columns = []
+    columns = st.multiselect('Specification(s):', df[['YEAR', 'COUNTRY', 'SITC 1 DIGIT', 'SITC 2 DIGIT']].drop(feature, axis = 1).columns,  key = '1')
+    try:
+        if ('SITC 1 DIGIT' in columns) & ('SITC 2 DIGIT' in columns):
+            st.write("You may only choose either one of SITC 1 DIGIT and SITC 2 DIGIT")
+        else:
+            if len(columns) == 0:
+                sort = st.selectbox('Sort by:', show, key='2')
+                single_graph_bar(feature, sort)
+            elif len(columns) == 1:
+                i = st.selectbox(columns[0] + ':', np.sort(df[columns[0]].unique()), key='3')
+                sort = st.selectbox('Sort by:', show, key='4')
+                bi_graph_bar(feature, columns[0], i, sort)
+            elif len(columns) == 2:
+                i = st.selectbox(columns[0] + ':', np.sort(df[columns[0]].unique()), key='5')
+                j = st.selectbox(columns[1] + ':', np.sort(df[columns[1]].unique()), key='6')
+                sort = st.selectbox('Sort by:', show, key='7')
+                multiple_graph_bar(feature, columns[0], i, columns[1], j, sort)
+    except IndexError:
+        st.error('No data available')
+
 st.subheader("Bar Graph Graph of Malaysia's Trade Perfomance")
+bar_graph()
