aa345c392bbadc4bbb3c5e9a85931cec1a30a6ec
Muhammad Hazim <hazimutalib96@gmail.com>
2020-11-29 19:51:58 +0800

    Create treemap1_app.py

---
+++ treemap1_app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+sns.set()
+import base64
+import plotly.express as px
+
+st.write(""" # Malaysia's Trade Performance Dashboard Application """)
+
+df = pd.read_csv("trade_new.csv")
+df_country = pd.read_csv("region.csv")
+
+feature = st.selectbox('Feature:', ['COUNTRY', 'SITC 1 DIGIT', 'SITC 2 DIGIT', 'YEAR'])
+metric = st.selectbox('Metric:', ['IMPORT (MILLION RM)','EXPORT (MILLION RM)'] )
+if (feature == 'SITC 1 DIGIT') or (feature == 'SITC 2 DIGIT'):
+    columns = st.sidebar.multiselect('Specification(s):', df[['COUNTRY', 'SITC 1 DIGIT', 'SITC 2 DIGIT', 'YEAR']].drop(
+        ['SITC 1 DIGIT', 'SITC 2 DIGIT'], axis=1).columns, key='52')
+else:
+    columns = st.sidebar.multiselect('Specification(s):', df[['COUNTRY', 'SITC 1 DIGIT', 'SITC 2 DIGIT', 'YEAR']].drop(feature, axis=1).columns,  key = '53')
+
+i = st.selectbox(columns + ':', np.sort(df[columns].unique()), key='3')
+
+if feature == 'COUNTRY':
+    df_group = df[df[columns] == i ].groupby('COUNTRY')[metric].sum().reset_index()
+    df_merge = df_group.merge(df_country, on='COUNTRY', how='inner')
+    df_merge = df_merge[df_merge[metric] > 0]
+    df_merge["WORLD"] = "WORLD"
+    fig = px.treemap(df_merge, path=['WORLD', 'Region', feature], values=metric, color=metric, hover_data=[metric], color_continuous_scale='RdBu')
+    st.plotly_chart(fig)
+elif feature == 'SITC 2 DIGIT':
+    df_merge = df[df[columns] == i ].groupby([feature,'SITC 1 DIGIT'])[metric].sum().reset_index()
+    df_merge = df_merge[df_merge[metric] > 0]
+    df_merge["COMMODITY"] = "COMMODITY"
+    fig = px.treemap(df_merge, path=['COMMODITY', 'SITC 1 DIGIT', feature], values=metric, color=metric, hover_data=[metric], color_continuous_scale='RdBu')
+    st.plotly_chart(fig)
+else:
+    fig = px.treemap(df[df[columns] == i], path=[feature], values=metric, color=metric, hover_data=[metric],color_continuous_scale='RdBu')
+    st.plotly_chart(fig)
